5473ff39b0d40fa2bf4cbb710798e96a6a6d649e
Billal Asif <asif.billal@outlook.com>
2023-03-07 00:45:41 +0000

    contact us complete

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
+// Get year
 document.getElementById("year").innerHTML = new Date().getFullYear();
+
+// owl carousel
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 3
+        }
+    }
+});
