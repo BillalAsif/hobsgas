bdeb9a91b9668021d31d8aab8b04f4c769327044
Billal Asif <asif.billal@outlook.com>
2023-03-08 09:52:01 +0000

    responsive started

--- js/script.js
+++ js/script.js
@@ -10,8 +10,8 @@ $('.owl-carousel').owlCarousel({
         0: {
             items: 1
         },
-        600: {
-            items: 3
+        767: {
+            items: 1
         },
         1000: {
             items: 3
